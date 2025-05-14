1c4f549c2dd38d58dd2163acfb7bf1e2530f645c
Megha Rana <megha.rana@fiery.com>
2025-05-13 22:58:39 -0700

    Addition of positive numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,6 +5,18 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it 'return the single number if only 1 number is passed' do 
+      expect(StringCalculator.add("5")).to eq(5)
+    end
+
+    it 'return addition of numbers if 2 numbers are passed' do
+      expect(StringCalculator.add("2,4")).to eq(6) 
+    end
+
+    it 'return addition of numbers if seperated by commas' do 
+      expect(StringCalculator.add("3,7,0")).to eq(10)
+    end
   end
 end
 

e97fec43cbf05f922b07a14afd9508e90e7bbf3a
Megha Rana <megha.rana@fiery.com>
2025-05-13 23:17:39 -0700

    Support for different delimeters and error negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -21,6 +21,14 @@ RSpec.describe StringCalculator do
     it 'return addition when numbers seperated by newline' do 
       expect(StringCalculator.add("3\n7,0")).to eq(10)
     end
+
+    it 'return addition when numbers support different delimiters' do 
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
+    
+    it 'return error for negative numbers' do 
+      expect { StringCalculator.add("1,-2,3,-4") }.to raise_error("Negatives not allowed: -2, -4")
+    end
   end
 end
 
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,20 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i if !numbers.include?(",")
-    numbers = numbers.gsub("\n", ",")
-    numbers.split(",").map(&:to_i).sum
+
+    delimiter = ","
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = parts[0][2]
+      numbers = parts[1]
+    end
+    
+    numbers = numbers.gsub("\n", delimiter)
+    nums = numbers.split(delimiter).map(&:to_i)
+
+    negatives = nums.select { |n| n < 0 }
+    raise "Negatives not allowed: #{negatives.join(', ')}" if negatives.any?
+
+    nums.sum
   end
 end
